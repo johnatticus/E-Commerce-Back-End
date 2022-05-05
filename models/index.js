c164f29caca5c1266d1bb6f92a4736a8dc954839
John Hysong <johnahysong@gmail.com>
2022-05-04 23:20:52 -0400

    finished index model, worked on category route

--- models/index.js
+++ models/index.js
@@ -5,6 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // Define the third table needed to store the foreign keys
+  foreignKey: 'category_id',
+  onDelete: 'CASECADE',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+})
+
+Location.belongsToMany(Traveller, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'location_travellers'
+});
 
 // Categories have many Products
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,6 +16,21 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      include: [{ model: Product }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
